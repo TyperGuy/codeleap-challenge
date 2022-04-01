a44537899ee073ec8db1ba5f909c0da2c670818f
TyperGuy <ray.developper@gmail.com>
2022-03-31 22:03:50 -0800

    chore: configuring eslint to work alongside prettier

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,8 @@
+
+
+function LandingPage() {
+  return 
+    <div style={{width: '100%', height: '70px', borderBottom: '1px solid #ebebeb', display: 'flex', alignItems: 'center' ,justifyContent: 'center'}} />
+}
+
+export default LandingPage;
