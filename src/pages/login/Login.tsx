5483ad9690116474401055a8d6fd542e82a687dd
TyperGuy <ray.developper@gmail.com>
2022-04-08 17:35:47 +0100

    feat: passing username using useContext hook

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,10 @@
-import React,{useState} from 'react'
+import React, { useContext, useState } from 'react'
 import { FlexType, ButtonType } from 'customTypes'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Container, LoginBox as Box } from '../../styles/Flex.styles'
 import Button from '../../components/button/Button'
 import { Label, InputField } from '../../styles/Input.styles'
+import { MyContext } from '../../routes/Routes'
 
 const FlexProps: FlexType = {
     justify: 'center',
@@ -20,37 +21,36 @@ const FlexChild: FlexType = {
     height: '',
 }
 
-
-
-
 const LoginPage: React.FC = () => {
-    const navegate = useNavigate();
-    const [disable,setisable] = useState(false);
-    function login(){
+    const navegate = useNavigate()
+    const [disable, setisable] = useState(false)
+    const context = useContext(MyContext)
+    function login() {
         navegate('/home')
     }
     const ButtonProps: ButtonType = {
         text: 'Enter',
         variant: '',
         disabled: !disable,
-        onClick:login
+        onClick: login,
     }
-    return(
+    return (
         <Container {...FlexProps}>
-        <Box>
-            <Container {...FlexChild}>
-                <h2>Welcome to CodeLeap network!</h2>
-                <Label>Please enter your username</Label>
-                <InputField onChange={(e)=>setisable(!!(e.target.value))} placeholder="Enter your username" />
-                <div style={{ alignSelf: 'end' }}>
-                    <Button button={ButtonProps} />
-                </div>
-            </Container>
-        </Box>
-    </Container>
-
+            <Box>
+                <Container {...FlexChild}>
+                    <h2>Welcome to CodeLeap network!</h2>
+                    <Label>Please enter your username sir {context}</Label>
+                    <InputField
+                        onChange={(e) => setisable(!!e.target.value)}
+                        placeholder="Enter your username"
+                    />
+                    <div style={{ alignSelf: 'end' }}>
+                        <Button button={ButtonProps} />
+                    </div>
+                </Container>
+            </Box>
+        </Container>
     )
-
 }
-    
+
 export default LoginPage
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,16 +1,23 @@
+import { createContext, useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import Login from '../pages/login/Login'
 
-const Routers = () => (
-    <div className="box">
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-            </Routes>
-        </Router>
-    </div>
-)
+export const MyContext = createContext('')
+const Routers = () => {
+    const [context, setContext] = useState('Raimundo')
+    return (
+        <div className="box">
+            <Router>
+                <MyContext.Provider value={context}>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/home" element={<Home />} />
+                    </Routes>
+                </MyContext.Provider>
+            </Router>
+        </div>
+    )
+}
 
 export default Routers
